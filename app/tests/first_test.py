9e9b319d0a54e958192353f72da62a62faf17971
pnehlin <per.nehlin@ip-solutions.se>
2025-02-11 11:47:23 +0100

    rapportera första felet

--- app/tests/first_test.py
+++ app/tests/first_test.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def test_locust_run():
-    result = subprocess.run(["locust", "-f", "load_test.py", "--headless", "-u", "10", "-r", "2", "--run-time", "10s"], capture_output=True, text=True)
+    result = subprocess.run(["locust", "--headless", "-u", "10", "-r", "2", "--run-time", "10s", "-H", "https://github.com/ipsper"], capture_output=True, text=True)
     assert "Requests/sec" in result.stdout, "Locust test did not run correctly"
